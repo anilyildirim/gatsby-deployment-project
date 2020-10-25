ff0603f4f3fd9d104f63b93ea8e00d749f76af86
Anil Yildirim <anilyildirim@yandex.com>
2020-10-25 08:21:24 +0100

    feat(gatsby-starter): add starter package

---
+++ gatsby-london-night-and-day/siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "London Night and Day",
+  shortName: "",
+  description: "This is the coolest text on earth",
+  url: "https://www.jooplaan.com", // Domain of your site without prefix!
+  prefix: "/",
+  author: "jooplaan",
+  twitter: ""
+};
